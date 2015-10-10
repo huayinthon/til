2a97d80dbc0e97e313d7c491ba92551d3339d32a
jasonatfleepio <ctchen@fleep.io>
2015-10-11 02:37:03 +0800

    add rubygems

--- packer/scripts/dep.sh
+++ packer/scripts/dep.sh
@@ -8,5 +8,6 @@ apt-get -y install curl
 
 # You can install anything you need here.
 
-apt-get install build-essential git ruby1.9.3 nodejs 
+apt-get install build-essential git ruby1.9.3 nodejs rubygems
+
 gem install jekyll jekyll-redirect-from rdiscount --no-ri --no-rdoc
