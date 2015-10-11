da0792258223341707df6807a01dab7aa85c1e31
jasonatfleepio <ctchen@fleep.io>
2015-10-11 10:58:23 +0800

    add ubuntu 14.04 LTS version

--- packer/scripts/dep.sh
+++ packer/scripts/dep.sh
@@ -8,7 +8,7 @@ apt-get -y install curl
 
 # You can install anything you need here.
 
-apt-get -y install build-essential git ruby1.9.3 nodejs rubygems screen
+apt-get -y install build-essential git ruby nodejs rubygems screen
 
 sudo gem install jekyll --no-ri --no-rdoc
 sudo gem install jekyll-redirect-from --no-ri --no-rdoc
