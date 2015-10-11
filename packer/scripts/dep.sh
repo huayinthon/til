2d0a5790df19d9302cae67bdb695ce6b1ebe4e68
jasonatfleepio <ctchen@fleep.io>
2015-10-11 11:33:00 +0800

    rubygems -> rubygems-integration

--- packer/scripts/dep.sh
+++ packer/scripts/dep.sh
@@ -8,7 +8,7 @@ apt-get -y install curl
 
 # You can install anything you need here.
 
-apt-get -y install build-essential git ruby nodejs rubygems screen
+apt-get -y install build-essential git ruby nodejs screen rubygems-integration
 
 sudo gem install jekyll --no-ri --no-rdoc
 sudo gem install jekyll-redirect-from --no-ri --no-rdoc
