9766fd893e6a4b54dcfca2bd32a08d002a0aa94f
zsyu <jsyu17@gmail.com>
2015-11-04 08:35:02 +0800

    revert

--- packer/scripts/dep.sh
+++ packer/scripts/dep.sh
@@ -13,7 +13,4 @@ apt-get -y install build-essential git ruby1.9.3 nodejs screen rubygems-integrat
 sudo gem install jekyll --no-ri --no-rdoc
 sudo gem install jekyll-redirect-from --no-ri --no-rdoc
 gem install rdiscount --no-ri --no-rdoc
-<<<<<<< HEAD
-=======
 
->>>>>>> 691ed1284a14cffcd6c843dd2d3bd288912ba84b
